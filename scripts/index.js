1c4b8f5cf7ab6a5dc6235147fe40e60eef43ab3a
Maksim Kurkov <angellone@yandex.ru>
2023-03-30 17:06:48 +0300

    Первое ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,4 @@
-/////Like's scripts/////
-
-/*const LikeButton = document.getElementsByClassName('element__like')
-
-/*for (var i = 0; i < LikeButton.length; i++) {
-    LikeButton[i].addEventListener('click', LikeChek);
-}
-
-function LikeChek() {
-    this.classList.toggle('element__like_active');
-}*/
-
-/////Popup's scripts /////
+/////Попап ппрофиля /////
 
 const popupElement = document.querySelector(".popup");
 const popupOpenButtonElement = document.querySelector(".profile__edit-button");
@@ -42,7 +30,110 @@ function popupFormSubmit(evt) {
     profileName.textContent = popupNameInput.value;
     profileStatus.textContent = popupStatusInput.value;
 
-    popupClose()
+    popupClose();
 }
 
-popupFormElement.addEventListener('submit', popupFormSubmit)
+popupFormElement.addEventListener('submit', popupFormSubmit);
+
+
+//Инициализация стандартного массива//
+const standartplaces = [
+    {
+        name: 'Камчатка',
+        link: 'https://images.unsplash.com/photo-1605653799416-595e2b020d08?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2067&q=80'
+    },
+    {
+        name: 'Кавказ',
+        link: 'https://images.unsplash.com/photo-1658648000143-c1b6c814221b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2002&q=80'
+    },
+    {
+        name: 'Урал',
+        link: 'https://images.unsplash.com/photo-1604250016566-75da99e8c4af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://images.unsplash.com/photo-1617835594990-7cd5a9b5d153?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+    },
+    {
+        name: 'Алтай',
+        link: 'https://images.unsplash.com/photo-1634206813058-44ccf5b33e50?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1351&q=80'
+    },
+    {
+        name: 'Карелия',
+        link: 'https://images.unsplash.com/photo-1630763741619-cc347d3cba55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+    }
+]
+
+const placeContainer = document.querySelector('.place-container');
+const placeTemplate = document.querySelector('.element-template').content;
+
+//обработка карточки темплейта//
+function renderItem(item) {
+    const placeElement = placeTemplate.cloneNode(true);
+    placeElement.querySelector('.element__name').textContent = item.name;
+    placeElement.querySelector('.element__photo').src = item.link;
+    const likeElement = placeElement.querySelector('.element__like');
+    const trashElement = placeElement.querySelector('.element__trash');
+    
+
+    //кнопка лайка//
+    likeElement.addEventListener('click', () => {
+        likeElement.classList.toggle('element__like_active');
+    });
+
+    //кнопка Удаления//
+    trashElement.addEventListener('click', () => {
+        placeElement.remove();
+    });
+
+    return placeElement;
+};
+
+//создание карточек для всех элементов//
+standartplaces.forEach(element => {
+    let newPlace = renderItem(element);
+    placeContainer.append(newPlace);
+});
+
+/////Добавление карточки из попапа /////
+
+const popupPlaceElement = document.querySelector(".popup-place");
+const popupPlaceOpenButtonElement = document.querySelector(".profile__add-button");
+const popupPlaceCloseButtonElement = popupPlaceElement.querySelector(".popup-place__exit");
+
+
+let popupPlaceFormElement = document.querySelector(".popup-place__form");
+
+let popupPlaceNameInput = popupPlaceFormElement.querySelector('.popup-place__input_type_place');
+let popupPlaceLinkInput = popupPlaceFormElement.querySelector('.popup-place__input_type_link');
+
+
+function popupPlaceActive() {
+    popupPlaceElement.classList.add('popup-place_opened');
+}
+
+popupPlaceOpenButtonElement.addEventListener('click', popupPlaceActive);
+
+function popupPlaceClose() {
+    popupPlaceElement.classList.remove('popup-place_opened');
+}
+
+popupPlaceCloseButtonElement.addEventListener('click', popupPlaceClose);
+
+//Добавление карточки//
+
+function addPlaceElement(evt) {
+    evt.preventDefault();
+
+    const addPlace = renderItem({
+        name: popupPlaceNameInput.value,
+        link: popupPlaceLinkInput.value
+    })
+    console.log(addPlace);
+    placeContainer.prepend(addPlace);
+    popupPlaceClose();
+    popupPlaceNameInput.value = '';
+    popupPlaceLinkInput.value = '';
+}
+
+popupPlaceFormElement.addEventListener('submit', addPlaceElement);
